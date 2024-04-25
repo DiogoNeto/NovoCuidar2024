4847f49aad8c2f067437da78985db5c677d289b2
Diogo Melo <diogo.melo@wish4wisdompt.onmicrosoft.com>
2024-04-25 23:00:02 +0100

    Atualizacao Modelo Dados 25 abril 2024

--- NovoCuidar2024/obj/Debug/net8.0/NovoCuidar2024.AssemblyInfo.cs
+++ NovoCuidar2024/obj/Debug/net8.0/NovoCuidar2024.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NovoCuidar2024")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb88df3e02d0add9621f4407a8ef569eb230f5ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8ccddfa213adeb08f81fe3e90623037727ec6ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("NovoCuidar2024")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NovoCuidar2024")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
