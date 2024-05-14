4c5105f4a874a30a3c5a125c6ecc74ba1336005c
Diogo <diogoncmc@gmail.com>
2024-05-14 23:15:00 +0100

    fix subsistemas on utentes

--- NovoCuidar2024/obj/Debug/net8.0/NovoCuidar2024.AssemblyInfo.cs
+++ NovoCuidar2024/obj/Debug/net8.0/NovoCuidar2024.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NovoCuidar2024")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7c59b6ce0a46ccf3d969e75902d92feb2161cb9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87303bde03684ef72026eb7cd11a78b1a1a36f38")]
 [assembly: System.Reflection.AssemblyProductAttribute("NovoCuidar2024")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NovoCuidar2024")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
