0ac92be97eed6b1db532185e28c12fe7c9deb1b3
Diogo <diogoncmc@gmail.com>
2024-06-03 08:35:59 +0100

    Edit identity register page

--- NovoCuidar2024/Program.cs
+++ NovoCuidar2024/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using NovoCuidar2024.Data;
 
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
